8135e2006ff4c62b482f989df32e965ab5378dfd
msamia <msamia@netsuite.com>
2016-01-19 17:32:56 +0100

    support for semantic versioning

--- make-rpm.mk
+++ make-rpm.mk
@@ -1,6 +1,11 @@
 # --- Variables ---
 PKGNAME=$(shell basename *.spec .spec)
-VERSION?=0.0.0# assign zeros only if not specified from cmdline by make {target} VERSION=1.2.3
+
+VERSION_MAJOR ?= 0
+VERSION_MINOR ?= 0
+VERSION_BUGFIX ?= 0
+
+VERSION?=$(VERSION_MAJOR).$(VERSION_MINOR).$(VERSION_BUGFIX)   # assign zeros only if not specified from cmdline by make {target} VERSION=1.2.3
 GROUP?=com.example# used when uploading to artifact repository
 WORKDIR:=/tmp/
 RELEASE=$(shell grep -oP '(?<=^Release: ).*' $(PKGNAME).spec | xargs rpm --eval)
